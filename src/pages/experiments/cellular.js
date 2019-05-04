fc726361cac5e1a49fd102982baba9434de61629
Nathan Englert <nathan.englert@newbalance.com>
2019-05-04 11:24:54 -0500

    Merged utility and cellular component due to gatsby build issues

--- src/pages/experiments/cellular.js
+++ src/pages/experiments/cellular.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import './cellular.scss';
 
-import CellularAutomata from './cellularAutomata';
+const TYPE_WALL = 1;
+const TYPE_FLOOR = 0;
 
 class Cellular extends React.Component {
   constructor (props) {
@@ -38,7 +39,51 @@ class Cellular extends React.Component {
       }
     })
 
-    this.generateMap(h, w);
+    this.generateMap(h, w, this.config.options);
+  }
+
+  countAliveNeighbours = (map, x, y, width, height) => {
+    let count = 0;
+
+    for (let i = -1; i < 2; i += 1) {
+        for (let j = -1; j < 2; j += 1) {
+            const neighbourX = x + i;
+            const neighbourY = y + j;
+
+            if (i === 0 && j === 0) continue;
+
+            if (neighbourX < 0 || neighbourY < 0 || 
+                neighbourX >= width || neighbourY >= height || 
+                map[neighbourX][neighbourY] === TYPE_WALL) {
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+  }
+
+  doSimulationStep = (oldMap, width, height, options) => {
+      const newMap = [];
+
+      for (let x = 0; x < width; x += 1) {
+        newMap.push([]);
+
+        for (var y = 0; y < height; y += 1) {
+          let nbs = this.countAliveNeighbours(oldMap, x, y, width, height);
+
+          if (oldMap[x][y] === TYPE_WALL) {
+            let type = nbs >= options.LIMIT_DEATH ? TYPE_WALL : TYPE_FLOOR;
+            newMap[x].push(type);
+          } 
+          else {
+            let type = nbs > options.LIMIT_BIRTH ? TYPE_WALL : TYPE_FLOOR;
+            newMap[x].push(type); 
+          }
+        }
+      }
+
+      return newMap;
   }
 
   drawMap = map => {
@@ -81,6 +126,48 @@ class Cellular extends React.Component {
   
     this.sendToCanvas(ctx, color, 1, 1, x, y);
   }
+
+  generateMap = (width, height, options) => {
+    const m = this;
+
+    let map = m.initialiseMap(width, height, options);
+    let i = 0;
+  
+    let interval = setInterval(function () {
+      map = m.doSimulationStep(map, width, height, options);
+  
+      let flatten = map.reduce((acc, val) => acc.concat(val), []);
+      m.drawMap(flatten);
+  
+      i += 1;
+      
+      if (i === options.NUMBER_OF_STEPS)
+        clearInterval(interval);
+    }, options.DRAW_SPEED)
+  
+    // for (var i = 0; i < options.NUMBER_OF_STEPS; i++)
+    // {
+      
+    // }
+  
+    // var flatten = map.reduce((acc, val) => acc.concat(val), []);
+    // return flatten;
+  }
+
+  initialiseMap = (width, height, options) => {
+    const map = [];
+
+    for (let x = 0; x < width; x += 1) {
+      map.push([]);
+
+      for (let y = 0; y < height; y += 1) {
+        let type = Math.random() < options.CHANCE_TO_START_ALIVE ? TYPE_WALL : TYPE_FLOOR;
+        map[x].push(type);
+      }
+    }
+    
+    return map;
+  }
   
   sendToCanvas = (ctx, pixels, height, width, x, y) => {
     const array = new Uint8ClampedArray(pixels);
@@ -90,10 +177,6 @@ class Cellular extends React.Component {
     y = parseInt(y);
     
     ctx.putImageData(image, x, y);        
-  }
-
-  generateMap (h, w) {
-    CellularAutomata.generate(h, w, this.config.options, this.drawMap);      
   }
 
   render () {
